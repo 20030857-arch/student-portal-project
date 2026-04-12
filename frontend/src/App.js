7b446fad81674760a3b2054e64df6ac8c558a2c7
69Beast-cmd <20031282@students.koi.edu.au>
2026-04-12 23:56:35 +1000

    Added assessment module (backend + frontend + database)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import React from "react";
+import AssessmentList from "./pages/AssessmentList";
 
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Cards from "./components/Cards";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-
-function AppLayout() {
-  const location = useLocation();
-
-  // ✅ hide navbar on login page
- const hideNavbar = ["/login", "/dashboard"].includes(location.pathname);
-
-
-  return (
-    <>
-      {!hideNavbar && <Navbar />}
-
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <Cards />
-          </>
-        } />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </>
-  );
+function App() {
+  return <AssessmentList />;
 }
 
-export default function App() {
-  return (
-    <Router>
-      <AppLayout />
-    </Router>
-  );
-}
+export default App;
